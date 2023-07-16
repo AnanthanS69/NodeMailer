3a6770fcd1a64e8067be5e6ddafb117a13ba2692
PRE4CH3R <92871683+AnanthanS69@users.noreply.github.com>
2023-07-16 13:29:38 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+const nodemailer=require('nodemailer');
+const transporter= nodemailer.createTransport({
+    service:'Gmail',
+    auth:{
+        user:'ananthan.s1969@gmail.com',
+        password: 'password'
+    }
+});
+const mailOptions = {
+    from: 'ananthan.s1969@gmail.com',
+    to: 'Password',
+    subject: 'Hello Friends!',
+    text: 'Hey friends, this is anemail sent using nodemailer.'
+  };
+  
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.error('Error sending email:', error);
+    } else {
+      console.log('Email sent:', info.response);
+    }
+  });
+
